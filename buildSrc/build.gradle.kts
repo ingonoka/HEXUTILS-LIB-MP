d3398fb55971b85926ea5ee04e8701167d53d8bb
ingonoka <ingonoka@icloud.com>
2019-09-16 13:14:21 +0800

    Remove unused code

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,33 +3,7 @@ import org.gradle.kotlin.dsl.`kotlin-dsl`
 plugins {
     `kotlin-dsl`
 }
-// Required since Gradle 4.10+.
-repositories {
-    jcenter()
-}
-/*
-buildscript {
-
-    repositories {
-        jcenter()
-        google()
-    }
-
-    dependencies {
-        // Kotlin
-        classpath(kotlin("gradle-plugin", version = "1.3.50"))
-    }
-}
 
 repositories {
     jcenter()
-    google()
 }
-
-plugins {
-  kotlin("kotlin-jvm")
-}
-
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-} */
