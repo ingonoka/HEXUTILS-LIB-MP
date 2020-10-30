66ea086299f65b92e180267b0ddce5b1da0be5ae
ingonoka <ingonoka@icloud.com>
2020-10-30 10:40:07 +0800

    Ad empty test to convince Gradle to run connected tests

---
+++ hexutilslib/src/androidTest/java/com/ingonoka/hexutils/EmptyTestToMakeConnectedAndroidTestWork.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018, 2019.  AF Payments and Ingo Noka.  This file belongs to project BERTLV.  It was written by Ingo Noka.
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ *
+ */
+
+package com.ingonoka.hexutils
+
