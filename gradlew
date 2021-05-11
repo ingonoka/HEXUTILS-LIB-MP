f3f8e831d8c149223a5d49205c3cc11066cb9847
ingonoka <ingonoka@icloud.com>
2021-05-11 12:52:14 +0800

    Update copyright notices

--- gradlew
+++ gradlew
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 
 #
+# Copyright (c) 2021. Ingo Noka
+# This file belongs to project hexutils-mp.
+# This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+#
+#
+
+#
 # Copyright 2015 the original author or authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- hexutils-lib/src/androidTest/java/com/ingonoka/hexutils/EmptyTestToMakeConnectedAndroidTestWork.kt
+++ hexutils-lib/src/androidTest/java/com/ingonoka/hexutils/EmptyTestToMakeConnectedAndroidTestWork.kt
@@ -1,4 +1,13 @@
 /*
+ * Copyright (c) 2021. Ingo Noka
+ * This file belongs to project hexutils-mp.
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ *
+ */
+
+/*
  * Copyright (c) 2018, 2019.  AF Payments and Ingo Noka.  This file belongs to project BERTLV.  It was written by Ingo Noka.
  *
  * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
